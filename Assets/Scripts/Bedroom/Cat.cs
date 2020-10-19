b48bb4925d09702515bd0c7dc98cdc42406ab361
Steve Hu <yh3133@nyu.edu>
2020-10-20 00:00:47 +0800

    commit 9, cat complete

---
+++ Assets/Scripts/Bedroom/Cat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    public GameObject ui;
+    public Sprite catstation;
+    public Sprite catted;
+    bool catCheck = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay2D(Collider2D activator)
+    {
+        if (!catCheck) {
+            ui.GetComponent<SpriteRenderer>().sprite = catstation;
+        }
+        else {
+            ui.GetComponent<SpriteRenderer>().sprite = catted;
+        }
+        if (Input.GetKey(KeyCode.F)) {
+            catCheck = true;
+        }
+    }
+
+     void OnTriggerExit2D()
+    {
+        catCheck = false;
+        ui.GetComponent<SpriteRenderer>().sprite = null;
+    }
+}
