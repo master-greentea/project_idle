0f70da71e6588a3295ceebf529405d513e0db6aa
Steven <yh3133@nyu.edu>
2020-10-19 18:41:26 +0800

    commit 7.95

---
+++ Assets/Scripts/Bedroom/CatSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSpawn : MonoBehaviour
+{
+    float x;
+    public GameObject cat;
+    // on first entering bedroom, maybe spawn cat
+    void Start()
+    {
+        x = Random.Range(0f, 100f);
+        // a 25 percent chance to spawn cat
+        if (x >= 75) {
+            cat.SetActive(true);
+        }
+    }
+}
