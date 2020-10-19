2981238787cf48a8c2bb9bb1c4c1d47c6dc98592
Steven <yh3133@nyu.edu>
2020-10-19 16:13:25 +0800

    commit 7.5, added guitar animation, 7 chords, NEEDS chord recording

---
+++ Assets/Scripts/Guitar/ChordPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChordPlayer : MonoBehaviour
+{
+    // put under individual chord
+    // animation
+    Animator myAni;
+    // chord audio
+    AudioSource myChord;
+    void Start()
+    {
+        myAni = GameObject.Find("String").GetComponent<Animator>();
+        myChord = GetComponent<AudioSource>();
+    }
+
+
+    void Update()
+    {
+        // stop animation if no chord is playing
+        if (Input.GetMouseButtonUp(0)) {
+            myAni.SetBool("playing", false);
+        }
+    }
+    void OnMouseDown()
+    {
+        // if chord playing, do animation
+        Debug.Log("click");
+        myAni.SetBool("playing", true);
+
+        // play chord
+        myChord.Play();
+    }
+
+}
