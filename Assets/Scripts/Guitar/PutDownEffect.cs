dc37604f75883eafe22e3963ac2969140c2366f3
Steven <yh3133@nyu.edu>
2020-10-19 14:42:17 +0800

    commit 7.2, guitar started

---
+++ Assets/Scripts/Guitar/PutDownEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutDownEffect : MonoBehaviour
+{
+    // put under put down to have bubble effect
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, .4f)-14.7f, -1.09f, -1);
+    }
+}
